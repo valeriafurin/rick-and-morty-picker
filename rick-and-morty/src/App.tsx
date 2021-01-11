f36c04c71ab7e0a792b5d0bc66094857276fad92
valeriafurin <v.furin@web.de>
2021-01-11 11:00:20 +0100

    Created repo. Initialized React App. Built Base Structure.

---
+++ rick-and-morty/src/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {Store} from './Store'
+
+export default function App():JSX.Element {
+  const {state, dispatch} = React.useContext(Store)
+
+ React.useEffect(() => {
+  state.episodes.length === 0 && fetchDataAction()
+ })
+
+ const fetchDataAction = async () => {
+   const URL = 'https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes'
+   const data = await fetch(URL)
+   const dataJSON = await data.json();
+   return dispatch({
+     type: 'FETCH_DATA',
+     payload: dataJSON._embedded.episodes
+   })
+ }
+
+ console.log(state)
+
+  return (
+    <React.Fragment>
+      <h1>Rick and Morty Season Picker</h1>
+      <p>Pick your favorite episode!</p>
+    </React.Fragment>
+  );
+}
---
+++ rick-and-morty/src/Store.tsx
@@ -0,0 +1,32 @@
+import React, { ReactElement, ReactComponentElement} from 'react'
+
+interface IState{
+    episodes: [],
+    favorites: []
+}
+
+interface IAction{
+    type: string,
+    payload: any
+}
+const initialState: IState = {
+    episodes: [],
+    favorites: []
+}
+
+export const Store = React.createContext<IState | any>(initialState)
+
+function reducer(state: IState, action: IAction): IState {
+    switch (action.type) {
+        case 'FETCH_DATA':
+            return { ...state, episodes: action.payload}
+        default:
+            return state
+
+    }
+}
+
+export function StoreProvider(props: any): JSX.Element {
+    const [state, dispatch] = React.useReducer(reducer, initialState)
+    return <Store.Provider value={{state, dispatch}}>{props.children}</Store.Provider>
+}
