8c78373b67af5f391ddf4cdec19c83c64cf3b0b9
valeriafurin <v.furin@web.de>
2021-01-11 13:58:43 +0100

    Fetched data from API to showcase all episodes

--- rick-and-morty/src/App.tsx
+++ rick-and-morty/src/App.tsx
@@ -1,6 +1,20 @@
 import React from 'react';
 import {Store} from './Store'
 
+interface IEpisode {
+ airdate: string,
+ airstamp: string,
+ airtime: string,
+ id: number,
+ image: {medium: string, original: string},
+ name: string,
+ number: number,
+ runtime: number,
+ season: number,
+ summary: string,
+ url: string
+}
+
 export default function App():JSX.Element {
   const {state, dispatch} = React.useContext(Store)
 
@@ -18,12 +32,23 @@ export default function App():JSX.Element {
    })
  }
 
- console.log(state)
-
   return (
     <React.Fragment>
       <h1>Rick and Morty Season Picker</h1>
       <p>Pick your favorite episode!</p>
+      <section>
+        {state.episodes.map((episode: IEpisode) => {
+          return (
+            <section key={episode.id}>
+              <img src={episode.image.medium} alt='Rick and Morty {episode.name}'/>
+              <div>{episode.name}</div>
+              <section>
+                Season: {episode.season} Number: {episode.number}
+              </section>
+            </section>
+          )
+        })}
+      </section>
     </React.Fragment>
   );
 }
